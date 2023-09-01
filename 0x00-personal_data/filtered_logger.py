3b50265c26082fed50be567506b5adadba7d411b
mahlet <akotet2123@gmail.com>
2023-09-01 15:02:04 +0000

    documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,10 +14,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ constructor method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ format method """
         log_message = super().format(record)
         return self.filter_datum(
                 self.fields,
