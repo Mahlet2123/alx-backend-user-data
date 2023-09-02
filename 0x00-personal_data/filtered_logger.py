381020ba6a14b95304445598944157715942b7c6
mahlet <akotet2123@gmail.com>
2023-09-02 15:49:38 +0000

    type annotation added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,7 +61,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     returns a connector to the database
     (mysql.connector.connection.MySQLConnection object).
