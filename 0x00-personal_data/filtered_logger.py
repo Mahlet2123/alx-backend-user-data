2a53a3b41e3f6cd409c07e19b87a0d79966f8548
mahlet <akotet2123@gmail.com>
2023-09-03 13:45:52 +0000

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,7 +69,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
     connector = mysql.connector.connect(
         host=db_host,
-        port=3306,
         user=db_user,
         password=db_pwd,
         database=db_name
