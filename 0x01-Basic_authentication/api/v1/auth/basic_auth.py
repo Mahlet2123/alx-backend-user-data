6c5ecfe27d3419372ad8457c2a94f47251db21c8
mahlet <akotet2123@gmail.com>
2023-09-05 16:43:02 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ basic_auth """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,22 @@ class BasicAuth(Auth):
         else:
             value = authorization_header.split(" ")
             return value[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if (
+            base64_authorization_header is None
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+        try:
+            decoded_value = base64.b64decode(base64_authorization_header)
+            return decoded_value.decode('utf-8')
+        except base64.binascii.Error:
+            return None
