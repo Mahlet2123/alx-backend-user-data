38806abc478e454a9bb9b1404a4856bd14b5f912
mahlet <akotet2123@gmail.com>
2023-09-05 17:19:20 +0000

    return the user email and the user password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,21 @@ class BasicAuth(Auth):
             return decoded_value.decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        returns the user email and password
+        from the Base64 decoded value.
+        """
+        if (
+            decoded_base64_authorization_header is None
+            or not isinstance(decoded_base64_authorization_header, str)
+            or ":" not in decoded_base64_authorization_header
+        ):
+            return None
+        else:
+            values = decoded_base64_authorization_header.split(":")
+            return (values[0], values[1])
