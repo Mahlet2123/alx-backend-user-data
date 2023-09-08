2ea6755045d2e425269f4c2f2688656e1a6abb32
mahlet <akotet2123@gmail.com>
2023-09-08 00:55:00 +0000

    sesssion auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -17,8 +18,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = getenv("AUTH_TYPE", None)
 
 if auth:
-    from api.v1.auth.auth import Auth
-    auth = BasicAuth()
+    auth = SessionAuth()
 
 
 @app.before_request
