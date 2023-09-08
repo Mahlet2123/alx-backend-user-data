cfc6f8a11c0e7b0517aa07ef6eb1dca32834e58c
mahlet <akotet2123@gmail.com>
2023-09-08 01:10:15 +0000

    PEP fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,23 +8,16 @@ class SessionAuth(Auth):
     """
     class SessionAuth that inherits from Auth
     """
+
     user_id_by_session_id = {}
 
-    def create_session(
-            self,
-            user_id: str = None
-            ) -> str:
+    def create_session(self, user_id: str = None) -> str:
         """
         creates a Session ID for a user_id
         """
-        if (
-                user_id is None
-                or not isinstance(user_id, str)
-            ):
+        if user_id is None or not isinstance(user_id, str):
             return None
         else:
             session_id = uuid.uuid4()
             self.user_id_by_session_id[str(session_id)] = user_id
             return session_id
-
-
